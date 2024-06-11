ff93dc4c5a3c8af7c1fed6d7df0c1e23077436b7
mswic <maciek.swic@gmail.com>
2024-06-11 17:23:45 +0200

    Testnet configuration

--- miner.config.js
+++ miner.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'miner',
       script: 'python3',
-      args: './neurons/miner.py --netuid 28 --logging.debug --logging.trace --subtensor.network local --wallet.name walletName --wallet.hotkey hotkeyName --axon.port 8091 --hf_repo_id foundryservices/bittensor-sn28-base-lstm --model mining_models/base_lstm_new.h5'
+      args: './neurons/miner.py --netuid 28 --logging.debug --logging.trace --subtensor.network test --wallet.name 28_test --wallet.hotkey 28_test00 --axon.port 10994 --hf_repo_id LOCAL --model mining_models/base_lstm_new.h5'
     },
   ],
 };
