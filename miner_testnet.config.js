744568ac2ee033a7e8ea13989093512ce687154f
mswic <maciek.swic@gmail.com>
2024-06-18 13:47:34 +0200

    Changes in config

---
+++ miner_testnet.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'miner',
+      script: 'python3',
+      args: './neurons/miner.py --netuid 93 --logging.debug --logging.trace --subtensor.network test --wallet.name 28_test --wallet.hotkey 28_test00 --axon.port 13000 --hf_repo_id LOCAL --model mining_models/base_lstm_new.h5'
+    },
+  ],
+};
+
