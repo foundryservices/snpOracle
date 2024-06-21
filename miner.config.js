b6df60790ba30a695c5efd23c217d5da29147192
mswic <maciek.swic@gmail.com>
2024-06-21 14:19:52 +0200

    Updated miner.config.js

--- miner.config.js
+++ miner.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'miner',
       script: 'python3',
-      args: './neurons/miner.py --netuid 28 --logging.debug --logging.trace --subtensor.network local --wallet.name 28a --wallet.hotkey 28a00 --axon.port 13500 --hf_repo_id LOCAL --model mining_models/base_lstm_new.h5 --subtensor.chain_endpoint ws://83.126.40.85:9944'
+      args: './neurons/miner.py --netuid 28 --logging.debug --logging.trace --subtensor.network local --wallet.name 28a --wallet.hotkey 28a00 --axon.port 13500 --hf_repo_id LOCAL --model base_miner/mining_models/base_lstm_new.h5 --subtensor.chain_endpoint ws://83.126.40.85:9944'
     },
   ],
 };
