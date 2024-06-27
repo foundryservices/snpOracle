f3697da6be581edb80e57a66a00d20a28ddb5967
mswic <maciek.swic@gmail.com>
2024-06-27 15:17:16 +0200

    Miner config update

--- miner.config.js
+++ miner.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: 'miner',
+      name: 'miner_arimax_01',
       script: 'python3',
-      args: './neurons/miner.py --netuid 28 --logging.debug --logging.trace --subtensor.network local --wallet.name 28a --wallet.hotkey 28a00 --axon.port 13500 --hf_repo_id LOCAL --model base_miner/mining_models/arimax_model.pkl --subtensor.chain_endpoint ws://83.126.40.85:9944'
+      args: './neurons/miner.py --netuid 28 --logging.debug --logging.trace --subtensor.network local --wallet.name 28a --wallet.hotkey 28a00 --axon.port 13500 --hf_repo_id LOCAL --model mining_models/arimax_model.pkl --subtensor.chain_endpoint ws://83.126.40.85:9944'
     },
   ],
 };
