b80c733a4702f13382a0a561edcdae5a916af8ff
hscott <hscott@foundrydigital.com>
2024-11-19 16:38:57 -0500

    moved update blocks code out of set weights loop

--- oracle/validators/weight_setter.py
+++ oracle/validators/weight_setter.py
@@ -149,11 +149,6 @@ class weight_setter:
                         "Failed to set weights this iteration with message:",
                         msg,
                     )
-            async with self.lock:
-                self.current_block = self.subtensor.get_current_block()
-                self.blocks_since_last_update = (
-                    self.current_block - node_query(self, "SubtensorModule", "LastUpdate", [self.config.netuid])[self.my_uid]
-                )
         except Exception as e:
             bt.logging.error(f"set_weights loop error: {e}")
             raise e
@@ -186,10 +181,11 @@ class weight_setter:
                     if self.config.wandb_on:
                         log_wandb(responses, rewards, self.available_uids)
                 else:
-                    self.current_block = node_query(self, "System", "Number", [])
-                    self.blocks_since_last_update = (
-                        self.current_block - node_query(self, "SubtensorModule", "LastUpdate", [self.config.netuid])[self.my_uid]
-                    )
+                    async with self.lock:
+                        self.current_block = node_query(self, "System", "Number", [])
+                        self.blocks_since_last_update = (
+                            self.current_block - node_query(self, "SubtensorModule", "LastUpdate", [self.config.netuid])[self.my_uid]
+                        )
                     print_info(self, "Market Open")
             else:
                 print_info(self, "Market Closed")
