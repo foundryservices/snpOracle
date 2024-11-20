6f6491339fe6a7e8c2d5fc1b1d2bb302aa493bb8
hscott <hscott@foundrydigital.com>
2024-11-20 09:41:20 -0500

    debug statements

--- oracle/validators/reward.py
+++ oracle/validators/reward.py
@@ -33,6 +33,8 @@ def calc_rewards(
             continue
         else:
             raw_deltas[uid, :, :], raw_correct_dir[uid, :, :] = calc_raw(prediction_dict, price_dict, response.timestamp, N_TIMEPOINTS=N_TIMEPOINTS)
+    bt.logging.info(f"example raw deltas: {raw_deltas[42,:,:]}")
+    bt.logging.info(f"example raw correct dir: {raw_correct_dir[42,:,:]}")
     for t in range(N_TIMEPOINTS):
         ranks[:, :, t] = rank_miners_by_epoch(raw_deltas[:, :, t], raw_correct_dir[:, :, t])
     bt.logging.info(f"ranks: {ranks}")
