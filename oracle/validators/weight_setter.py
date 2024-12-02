940c29370ba1905a9c4335dbcf4a05c014f9421c
hscott <hscott@foundrydigital.com>
2024-12-02 15:00:20 -0500

    fixed resync metagraph issue where registering a hotkey bugs the scores allocation

--- oracle/validators/weight_setter.py
+++ oracle/validators/weight_setter.py
@@ -1,21 +1,28 @@
 import asyncio
-from aiohttp import ClientConnectorError
 import os
 import pickle
 
 import bittensor as bt
+from aiohttp import ClientConnectorError
 from numpy import array, isnan
 from substrateinterface import SubstrateInterface
 
-
 from oracle import __spec_version__
 from oracle.protocol import Challenge
-from oracle.utils.bittensor import get_available_uids, print_info, setup_bittensor_objects, node_query
-from oracle.utils.timestamp import market_is_open, is_query_time, elapsed_seconds, get_now, get_before, iso8601_to_datetime
-from oracle.utils.wandb import setup_wandb, log_wandb
+from oracle.utils.bittensor import get_available_uids, node_query, print_info, setup_bittensor_objects
 from oracle.utils.classes import MinerHistory
 from oracle.utils.general import loop_handler
+from oracle.utils.timestamp import (
+    elapsed_seconds,
+    get_before,
+    get_now,
+    is_query_time,
+    iso8601_to_datetime,
+    market_is_open,
+)
+from oracle.utils.wandb import log_wandb, setup_wandb
 from oracle.validators.reward import calc_rewards
+
 
 class weight_setter:
     def __init__(self, config=None, loop=None):
@@ -53,9 +60,7 @@ class weight_setter:
         self.resync_event = asyncio.Event()
         self.set_weights_event = asyncio.Event()
         bt.logging.info("Setup complete, starting loop")
-        self.loop.create_task(
-            loop_handler(self, self.main_function, sleep_time=self.config.print_cadence)
-        )
+        self.loop.create_task(loop_handler(self, self.main_function, sleep_time=self.config.print_cadence))
         self.loop.create_task(loop_handler(self, self.resync_metagraph, sleep_time=self.resync_metagraph_rate))
         self.loop.create_task(loop_handler(self, self.set_weights, sleep_time=self.set_weights_rate))
 
@@ -69,7 +74,6 @@ class weight_setter:
         except Exception as e:
             bt.logging.error(f"Error on __exit__ function: {e}")
         self.loop.stop()
-
 
     async def resync_metagraph(self):
         """Resyncs the metagraph and updates the hotkeys and moving averages based on the new metagraph."""
@@ -87,9 +91,9 @@ class weight_setter:
                     if (uid not in self.MinerHistory and uid in self.available_uids) or self.hotkeys[uid] != hotkey:
                         bt.logging.info(f"Replacing hotkey on {uid} with {self.metagraph.hotkeys[uid]}")
                         self.hotkeys[uid] = hotkey
-                        self.scores[uid] = 0  # hotkey has been replaced
                         self.MinerHistory[uid] = MinerHistory(uid)
                         self.moving_average_scores[uid] = 0
+                        self.scores = array(list(self.moving_average_scores.values()))
                 self.last_sync = self.subtensor.get_current_block()
                 self.save_state()
                 self.resync_event.set()
@@ -118,7 +122,6 @@ class weight_setter:
             bt.logging.debug(f"Connect issues: {connect_issues} | Timeouts: {Timeouts}")
         return responses
 
-
     async def set_weights(self):
         try:
             if self.blocks_since_last_update >= self.set_weights_rate:
@@ -132,7 +135,7 @@ class weight_setter:
                     bt.logging.debug(f"UID: {j}  |  Weight: {i}")
                 if sum(weights) == 0:
                     weights = [1] * len(weights)
-                weights = array(weights)/max(weights)
+                weights = array(weights) / max(weights)
                 # Convert to uint16 weights and uids.
                 (
                     uint_uids,
@@ -156,7 +159,8 @@ class weight_setter:
                     )
             self.current_block = node_query(self, "System", "Number", [])
             self.blocks_since_last_update = (
-                self.current_block - node_query(self, "SubtensorModule", "LastUpdate", [self.config.netuid])[self.my_uid]
+                self.current_block
+                - node_query(self, "SubtensorModule", "LastUpdate", [self.config.netuid])[self.my_uid]
             )
             self.set_weights_event.set()
         except Exception as e:
@@ -164,39 +168,36 @@ class weight_setter:
             raise e
 
     async def main_function(self):
-        try:
-            if market_is_open():
-                query_lag = elapsed_seconds(get_now(), self.timestamp)
-                if is_query_time(self.prediction_interval, self.timestamp) or query_lag >= 60 * self.prediction_interval:
-                    responses = self.query_miners()
-                    self.timestamp = iso8601_to_datetime(responses[0].timestamp)
-                    try:
-                        rewards = calc_rewards(
-                            self,
-                            responses=responses,
-                        )
-                    except Exception as e:
-                        bt.logging.error(f"Error calculating rewards: {e}")
-                        raise e
-                    for uid, response, reward in zip(self.available_uids, responses, rewards):
-                        if response.prediction is not None:
-                            bt.logging.info(f"UID: {uid}  |  Prediction: {response.prediction}  |  Reward: {reward}")
-                    # Adjust the scores based on responses from miners and update moving average.
-                    async with self.lock:
-                        for i, value in zip(self.available_uids, rewards):
-                            self.moving_average_scores[i] = (1 - self.config.alpha) * self.moving_average_scores[
-                                i
-                            ] + self.config.alpha * value
-                        self.scores = array(list(self.moving_average_scores.values()))
-                        bt.logging.debug(f"Scores: {self.scores}")
-                    if self.config.wandb_on:
-                        log_wandb(responses, rewards, self.available_uids)
-                else:
-                    print_info(self, "Market Open")
+        if market_is_open():
+            query_lag = elapsed_seconds(get_now(), self.timestamp)
+            if is_query_time(self.prediction_interval, self.timestamp) or query_lag >= 60 * self.prediction_interval:
+                responses = self.query_miners()
+                self.timestamp = iso8601_to_datetime(responses[0].timestamp)
+                try:
+                    rewards = calc_rewards(
+                        self,
+                        responses=responses,
+                    )
+                except Exception as e:
+                    bt.logging.error(f"Error calculating rewards: {e}")
+                    raise e
+                for uid, response, reward in zip(self.available_uids, responses, rewards):
+                    if response.prediction is not None:
+                        bt.logging.info(f"UID: {uid}  |  Prediction: {response.prediction}  |  Reward: {reward}")
+                # Adjust the scores based on responses from miners and update moving average.
+                async with self.lock:
+                    for i, value in zip(self.available_uids, rewards):
+                        self.moving_average_scores[i] = (1 - self.config.alpha) * self.moving_average_scores[
+                            i
+                        ] + self.config.alpha * value
+                    self.scores = array(list(self.moving_average_scores.values()))
+                    bt.logging.debug(f"Scores: {self.scores}")
+                if self.config.wandb_on:
+                    log_wandb(responses, rewards, self.available_uids)
             else:
-                print_info(self, "Market Closed")
-        except Exception as e:
-                bt.logging.error(f"main loop error: {e}")
+                print_info(self, "Market Open")
+        else:
+            print_info(self, "Market Closed")
 
     def save_state(self):
         """Saves the state of the validator to a file."""
