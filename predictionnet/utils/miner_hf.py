206190710782e218c1aea15b94de4082c014476d
root <root@DCGF-47X6N44-W.>
2024-12-02 15:05:37 -0500

    Created miner_hf, a util file for miner huggingface functions. Created upload model functionality

---
+++ predictionnet/utils/miner_hf.py
@@ -0,0 +1,32 @@
+from huggingface_hub import HfApi, model_info, metadata_update
+from dotenv import load_dotenv
+import os
+import time
+
+class Miner_HF_interface:
+    def __init__(self):
+        load_dotenv()
+        self.api = HfApi(token=os.getenv("MINER_HF_ACCESS_TOKEN"))
+
+    def upload_model(self, repo_id, model_path, hotkey):
+        if not all([repo_id, model_path, hotkey]):
+            raise ValueError("All parameters (repo_id, model_path, hotkey) must be specified")
+        
+        try:
+            timestamp = str(int(time.time()))
+            self.api.upload_file(
+                path_or_fileobj=model_path,
+                path_in_repo="model.safetensors", 
+                repo_id=repo_id,
+                repo_type="model"
+            )
+            
+            metadata = {
+                "hotkey": hotkey,
+                "timestamp": timestamp
+            }
+            metadata_update(repo_id, metadata)
+            return True, {"hotkey": hotkey, "timestamp": timestamp}
+            
+        except Exception as e:
+            return False, str(e)
