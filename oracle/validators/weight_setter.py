eb491d84446ecac97bd3accee7dd52d33e1b2317
hscott <hscott@foundrydigital.com>
2024-12-03 09:36:33 -0500

    code at wrong depth after lock removal

--- oracle/validators/weight_setter.py
+++ oracle/validators/weight_setter.py
@@ -182,8 +182,8 @@ class weight_setter:
                     self.moving_average_scores[i] = (1 - self.config.alpha) * self.moving_average_scores[
                         i
                     ] + self.config.alpha * value
-                    self.scores = array(list(self.moving_average_scores.values()))
-                    bt.logging.debug(f"Scores: {self.scores}")
+                self.scores = array(list(self.moving_average_scores.values()))
+                bt.logging.debug(f"Scores: {self.scores}")
                 if self.config.wandb_on:
                     log_wandb(responses, rewards, self.available_uids)
             else:
