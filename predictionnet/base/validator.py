d82c34231f05033ff34bc58ae059dc5f53a65de4
hscott <hscott@foundrydigital.com>
2024-12-04 11:45:30 -0500

    metagraph length error

--- predictionnet/base/validator.py
+++ predictionnet/base/validator.py
@@ -299,9 +299,10 @@ class BaseValidatorNeuron(BaseNeuron):
             rewards = nan_to_num(rewards, 0)
         # Compute forward pass rewards, assumes uids are mutually exclusive.
         # shape: [ metagraph.n ]
+        moving_avg_scores = self.scores.copy()
         for i, value in zip(uids, rewards):
-            self.moving_avg_scores[i] = (1 - self.alpha) * self.scores[i] + self.alpha * value
-        self.scores = array(self.moving_avg_scores)
+            moving_avg_scores[i] = (1 - self.alpha) * self.scores[i] + self.alpha * value
+        self.scores = array(moving_avg_scores)
         bt.logging.info(f"New Average Scores: {self.scores}")
 
     def save_state(self):
